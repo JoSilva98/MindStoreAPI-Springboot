a024cb1418faf929d63ce13e114a3a2a8c868e9c
jon <joao.couto@mindswap.academy>
2022-08-10 18:29:11 +0100

    externalAPI

---
+++ src/main/java/MindStore/externalapi/ExternalAPIController.java
@@ -0,0 +1,14 @@
+package MindStore.externalapi;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class ExternalAPIController {
+
+}
