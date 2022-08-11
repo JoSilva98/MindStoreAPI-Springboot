cd85117c0e0f69bbb7793f404d0b0b548c2c3eb9
jon <joao.couto@mindswap.academy>
2022-08-11 17:08:30 +0100

    user dto

--- src/main/java/MindStore/command/UserDto.java
+++ src/main/java/MindStore/command/UserDto.java
@@ -4,16 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserDto {
+@SuperBuilder
+public class UserDto extends PersonDto{
+
+    @NotEmpty
+    @Size(min = 5, max = 40, message = "Adress should have at least 5 characters")
+    private String address;
 
     @NotNull //qd datetimeformat em vez de notempty
     @DateTimeFormat
