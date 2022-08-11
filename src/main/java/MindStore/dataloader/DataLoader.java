174014ecbf6d12690b856507a5a52c7f9e9ec261
jon <joao.couto@mindswap.academy>
2022-08-11 16:06:00 +0100

    Changes by Utilizador

--- src/main/java/MindStore/dataloader/DataLoader.java
+++ src/main/java/MindStore/dataloader/DataLoader.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.List;
 
 @RequiredArgsConstructor
