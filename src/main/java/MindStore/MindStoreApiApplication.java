a5fe706595d3213a96ae5376e3854533fcb2ef90
jon <joao.couto@mindswap.academy>
2022-08-14 20:25:34 +0100

    cross origin testing

--- src/main/java/MindStore/MindStoreApiApplication.java
+++ src/main/java/MindStore/MindStoreApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MindStoreApiApplication {
-//
+
 	public static void main(String[] args) {
 		SpringApplication.run(MindStoreApiApplication.class, args);
 	}
