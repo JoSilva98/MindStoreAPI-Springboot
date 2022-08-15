78db8cd4cd39af12827c007b68d3673a52badbed
Utilizador <johnny.61576.js@gmail.com>
2022-08-15 18:32:58 +0100

    Testing DataLoader

--- src/main/java/MindStore/dataloader/DataLoader.java
+++ src/main/java/MindStore/dataloader/DataLoader.java
@@ -43,7 +43,6 @@ public class DataLoader implements ApplicationRunner {
     private final PasswordEncoder encoder;
 
     @Override
-    //@CacheEvict(allEntries = true)
     public void run(ApplicationArguments args) {
         if (!this.productRepository.findAllByRatingASC(5, 0).isEmpty())
             return;
