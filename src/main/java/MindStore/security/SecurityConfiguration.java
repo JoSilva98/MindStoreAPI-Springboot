fbcc348a40ecf47051228753263b0b76cec818a1
Utilizador <johnny.61576.js@gmail.com>
2022-08-15 08:19:07 +0100

    Swagger

--- src/main/java/MindStore/security/SecurityConfiguration.java
+++ src/main/java/MindStore/security/SecurityConfiguration.java
@@ -38,6 +38,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .addFilter(new JwtAuthorizationFilter(authenticationManager(), this.personRepository))
                 .authorizeRequests()
+                .antMatchers("/v2/api-docs", "/swagger-resources", "/swagger-resources/**",
+                        "/configuration/ui", "/configuration/security", "/swagger-ui.html",
+                        "/webjars/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                 .antMatchers("/api/v1/users/products/**").permitAll()
